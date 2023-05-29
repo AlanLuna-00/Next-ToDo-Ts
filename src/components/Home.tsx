933045796f8f4f05e50e45bccd2e1cb257847506
AlanLuna-00 <alan.luna.desarrollo@gmail.com>
2023-05-29 16:56:41 -0300

    change to redux WTF

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,22 @@
-"use client"
-import React from "react";
-import { Button, Container } from "@chakra-ui/react";
+"use client";
+import React, { useState } from "react";
+import { Box, Button, Flex, Container } from "@chakra-ui/react";
 import CreateTaskModal from "@/components/CreateTaskModal";
-import ShowTodos from './ShowTodo';
+import ShowTodos from "./ShowTodo";
+import { Task } from "@/redux/taskReducer";
 
 const Home = () => {
-  const [isModalOpen, setIsModalOpen] = React.useState(false);
+  const days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedDay, setSelectedDay] = useState("");
 
   const handleOpenModal = () => {
     setIsModalOpen(true);
@@ -15,13 +26,33 @@ const Home = () => {
     setIsModalOpen(false);
   };
 
+  const handleDayClick = (day: string) => {
+    setSelectedDay(day);
+  };
+
   return (
-    <Container maxW="md" id="tasks">
-      <Button onClick={handleOpenModal}>Create Task
-        <CreateTaskModal isOpen={isModalOpen} onClose={handleCloseModal} />
-        </Button>
-        <ShowTodos />
-    </Container>
+    <div className="taskContainer" id="tasks">
+      <Flex>
+        <Box bg="gray.800" h="100vh" w="200px" p="4">
+          {days.map((day) => (
+            <Button
+              key={day}
+              onClick={() => handleDayClick(day)}
+              backgroundColor={selectedDay === day ? "gray.100" : "gray.700"}
+              variant="ghost"
+              marginTop="10px"
+            >
+              {day}
+            </Button>
+          ))}
+        </Box>
+        <Box flex="1" p="4">
+          <Button onClick={handleOpenModal}>Create Task</Button>
+          <CreateTaskModal isOpen={isModalOpen} onClose={handleCloseModal} />
+          <ShowTodos selectedDay={selectedDay} />
+        </Box>
+      </Flex>
+    </div>
   );
 };
 
